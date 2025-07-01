5fdf0d1c1050bd3f443fb97e48c4515c3936f5e9
PatellaNitinSai <nitinsaipatella2024@gmail.com>
2025-07-01 17:07:41 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import EventList from './components/EventList';
